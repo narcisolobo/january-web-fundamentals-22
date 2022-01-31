5ff68c9dd57e662946624eb1a63f309af3345d78
narcisolobo <narcisolobo@gmail.com>
2022-01-31 11:34:37 -0600

    added DOM deeper dive notes

---
+++ JS/DomDeeperDive/domDeeperDive.js
@@ -0,0 +1,37 @@
+var title = document.querySelector('#title');
+var box = document.getElementById('box');
+// console.log(box);
+var counter = 0;
+box.innerText = counter
+
+function display(element){
+  console.log(element.innerText);
+}
+
+function changeText(element){
+  element.innerText = "You clicked me."
+}
+
+function changeBg(){
+  title.style.backgroundColor = "green"
+}
+
+function hoverOver(element) {
+  element.style.backgroundColor = "rebeccapurple"
+}
+
+function hoverOut(element) {
+  element.style.backgroundColor = "coral"
+}
+
+function increment(){
+  counter++;
+  box.innerText = counter;
+}
+
+function changePic(element){
+  var temp = element.src
+  element.src = element.getAttribute('altsrc')
+  element.setAttribute('altsrc', temp)
+  console.log(element);
+}
