55c51282798c4e987f9bddf64d570b5084f4aeff
narcisolobo <narcisolobo@gmail.com>
2022-02-03 15:09:11 -0600

    added pm notes

---
+++ terminalLecture/basic13.js
@@ -0,0 +1,11 @@
+// Print 1-255
+// Print1To255()
+// Print all the integers from 1 to 255.
+
+function print1To255(){
+  for(var i = 1; i <= 255; i++){
+    console.log(i);
+  }
+}
+
+print1To255();
